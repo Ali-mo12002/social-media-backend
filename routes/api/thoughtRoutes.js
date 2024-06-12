83dfc068cc60a5c9749e275123920ff742db6e66
Ali <iodk123@outlook.com>
2024-06-12 07:25:16 +0100

    added thought route

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -1,1 +1,39 @@
 
+const express = require('express');
+const {
+ getAllThoughts,
+ getSpecificThought,
+ createNewThought,
+ updateThought,
+ deleteThought,
+ addNewReaction,
+ removeReaction,
+} = require('../../controllers/thoughtController');
+
+
+const router = express.Router();
+
+
+router.route('/')
+ .get(getAllThoughts)
+ .post(createNewThought);
+
+
+router.route('/:thoughtId')
+ .get(getSpecificThought)
+ .put(updateThought)
+ .delete(deleteThought);
+
+
+router.route('/:thoughtId/reactions')
+ .post(addNewReaction);
+
+
+router.route('/:thoughtId/reactions/:reactionId')
+ .delete(removeReaction);
+
+
+module.export = router;
+
+
+
